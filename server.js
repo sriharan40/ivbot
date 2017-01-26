8db3a10e37fd85b9a0eb68d4b3374b7a6ecadec5
sriharan40 <sriharan40@gmail.com>
2017-01-27 02:30:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http'),
 http.createServer(function (req, res) {
     //Create TwiML response
     var twiml = new twilio.TwimlResponse();
-    twiml.say("Hello from your Himants POC ! Have fun. Bot options will be configured ASAP.");
+    twiml.say("Hello my Amdocs friends and family. This is Himants POC for IVR on cloud. Bot options will be configured ASAP.");
 
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
